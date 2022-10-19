e7fb95e88e4a6fa802756944d58a2ce4088efd52
matthew <matthewbeaulieu10@gmail.com>
2022-10-19 18:57:25 -0500

    init map and marker working, almost recreated lite

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,14 @@
 function letMeKnowImWorking () {
     console.log("working!")
 }
+
+var actualMap;
+
+function initMap() {
+    actualMap = new google.maps.Map(document.getElementById("mapDiv"), {
+        center: {lat:0,lng:0},
+        zoom:2,
+    });
+
+    var homeMarker = new google.maps.Marker({position:{lat:41.970760,lng:-88.351590},map:actualMap});
+}
